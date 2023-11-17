1b57ca7b7159990f39226878e5dceb1530ef33d7
Cinthia <87044276+today-is-cinthia@users.noreply.github.com>
2023-11-16 22:30:08 -0600

    finished example

---
+++ Stripe1/FLASK/app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, url_for, request, abort
+
+import stripe
+
+app = Flask(__name__)
+
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51ODIPSKRvWZtCW7xhdmXO2vp558FUBwAZd2yzVmU19f1JOmiIs44tM9ieQfnk4H45cINDWVDTjDqLyORjaEjTnrB002J108MtT'
+app.config['STRIPE_SECRET_KEY'] = 'sk_test_51ODIPSKRvWZtCW7xRDNE4rrtDOXE9Vx2LcioU0vsMTDEe3Hpow70WzQnInpLOiBKrSy1IHQbsYVYIl5mJt09wPGm008KopcXKc'
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
+@app.route('/')
+def index():
+    '''
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1GtKWtIdX0gthvYPm4fJgrOr',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True),
+    )
+    '''
+    return render_template(
+        'index.html', 
+        #checkout_session_id=session['id'], 
+        #checkout_public_key=app.config['STRIPE_PUBLIC_KEY']
+    )
+
+@app.route('/stripe_pay')
+def stripe_pay():
+    prices={'price':'price_1ODJCpKRvWZtCW7xkXtMDrzh', 'price':'price_1ODIyqKRvWZtCW7xYMryCKRB'}
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1ODJCpKRvWZtCW7xkXtMDrzh',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True),
+    )
+    return {
+        'checkout_session_id': session['id'], 
+        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
+    }
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+@app.route('/stripe_webhook', methods=['POST'])
+def stripe_webhook():
+    print('WEBHOOK CALLED')
+
+    if request.content_length > 1024 * 1024:
+        print('REQUEST TOO BIG')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = 'YOUR_ENDPOINT_SECRET'
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
+        print(line_items['data'][0]['description'])
+
+    return {}
